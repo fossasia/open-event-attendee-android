8da2247933aa982fa373b88790b8fe91ab8117ff
Rafał <rafal.arte@gmail.com>
2015-05-01 17:55:46 +0200

    Republica Api Urls added

---
+++ app/src/main/java/org/republica/api/RepublicaUrls.java
@@ -0,0 +1,26 @@
+package org.republica.api;
+
+/**
+ * Created by rafal on 01.05.2015.
+ */
+public class RepublicaUrls {
+
+    public static final String SESSIONS_URL = "http://data.re-publica.de/data/rp15/sessions.json";
+
+    public static final String SPEAKERS_URL = "http://data.re-publica.de/api/rp15/speakers";
+
+    public static final String TRACKS_URL = "http://data.re-publica.de/api/rp15/tracks";
+
+    public static final String LOCATIONS_URL = "http://data.re-publica.de/api/rp15/locations";
+
+    public static final String MAPS_URL = "http://data.re-publica.de/api/rp15/maps";
+
+    public static final String POIS_URL = "http://data.re-publica.de/api/rp15/pois";
+
+    public static final String FORMATS_URL = "http://data.re-publica.de/api/rp15/formats";
+
+    public static final String LEVELS_URL = "http://data.re-publica.de/api/rp15/levels";
+
+    public static final String LANGUAGES_URL = "http://data.re-publica.de/api/rp15/languages";
+
+}
