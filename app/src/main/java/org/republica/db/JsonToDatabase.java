1cb6f96468694b26eeb352e7f6a1f9f710afadbb
Rafał <rafal.arte@gmail.com>
2015-05-03 20:17:26 +0200

    JsonToDatabase Refactored

--- app/src/main/java/org/republica/db/JsonToDatabase.java
+++ app/src/main/java/org/republica/db/JsonToDatabase.java
@@ -46,11 +46,11 @@ public class JsonToDatabase {
 
 
     public void startDataDownload() {
-        parseEvents(RepublicaUrls.SESSIONS_URL);
+        parse(RepublicaUrls.DATA_URL);
 
     }
 
-    public void parseEvents(final String url) {
+    public void parse(final String url) {
         RequestQueue queue = VolleySingleton.getReqQueue(context);
 
         //Request string reponse from the url
@@ -61,109 +61,17 @@ public class JsonToDatabase {
 
                 ArrayList<String> queries = new ArrayList<>();
 
-                long id;
-                String title;
-                String subTitle;
-                ArrayList<String> keyNoteList;
-                //TODO: Do validation on date and convert to date object;
-                String date;
-                //TODO: Get day from Date object
-                String day;
-                //TODO: Convert time to date object, will be later used to save as reminder
-                String startTime;
-                String abstractText;
-                String description;
-                String venue;
-                String track;
-                String moderator;
-
                 try {
-                    JSONObject jObjMain = new JSONObject(response);
-                    JSONArray jsonArray = jObjMain.getJSONArray("sessions");
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        try {
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            id = i;
-                            title = jsonObject.getString("title");
-                            abstractText = jsonObject.getString("abstract");
-                            subTitle = jsonObject.getString("type");
+                    JSONObject jsonData = new JSONObject(response);
 
-                            JSONObject dayObject = jsonObject.getJSONObject("day");
-                            date = dayObject.getString("label_en");
-                            startTime = "1:00 AM";
-                            description = jsonObject.getString("description");
-                            JSONObject venueObject = jsonObject.getJSONObject("location");
-                            venue = venueObject.getString("label_en");
-                            track = jsonObject.getJSONObject("track").getString("label_en");
-                            moderator = "";
-                            FossasiaEvent event = new FossasiaEvent(i, title, subTitle, date, "", startTime, abstractText, description, venue, track, moderator);
-
-                            String query = "INSERT INTO %s VALUES ('%s', '%s', '%s');";
-
-                            JSONArray jArray = jsonObject.getJSONArray("speakers");
-                            for (int j = 0; j < jArray.length(); j++) {
-                                JSONObject jObj = jArray.getJSONObject(j);
-                                String fullName = jObj.getString("name");
-                                String speakerId = jObj.getString("id");
-                                String speakerQuery = String.format(query, DatabaseHelper.TABLE_NAME_SPEAKER_EVENT_RELATION, fullName, speakerId, StringUtils.replaceUnicode(title));
-                                queries.add(speakerQuery);
-
-                            }
-
-                            queries.add(event.generateSqlQuery());
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    }
-
-                    jsonArray = jObjMain.getJSONArray("speakers");
-
-                    String name;
-                    String information;
-                    String linkedInUrl;
-                    String twitterHandle;
-                    String designation;
-                    String profilePicUrl;
-                    String organization;
-                    boolean isKeySpeaker;
-
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        try {
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            id = i;
-                            name = jsonObject.getString("name");
-                            information = jsonObject.getString("biography");
-                            designation = jsonObject.getString("position");
-                            organization = jsonObject.getString("organization");
-
-                            if (organization != null && !organization.equals("")) {
-                                designation += ", " + organization;
-                            }
-                            profilePicUrl = jsonObject.getString("photo");
-                            Speaker speaker = new Speaker(i, name, information, "", "", designation, profilePicUrl, 0);
-                            queries.add(speaker.generateSqlQuery());
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-
-                    jsonArray = jObjMain.getJSONArray("tracks");
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        String trackName = jsonArray.getJSONObject(i).getString("label_en");
-
-                        String query = "INSERT INTO %s VALUES (%d, '%s', '%s');";
-                        query = String.format(query, DatabaseHelper.TABLE_NAME_TRACK, i, StringUtils.replaceUnicode(trackName), "");
-                        queries.add(query);
-                    }
+                    parseSession(jsonData);
+                    parseSpeakers(jsonData);
+                    parseTracks(jsonData);
 
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
 
                 DatabaseManager dbManager = DatabaseManager.getInstance();
                 //Temporary clearing database for testing only
@@ -191,6 +99,106 @@ public class JsonToDatabase {
 
     public static interface JsonToDatabaseCallback {
         public void onDataLoaded();
+    }
+
+    private void parseSession(JSONObject jsonData) throws JSONException{
+        long id;
+        String title;
+        String subTitle;
+        ArrayList<String> keyNoteList;
+        //TODO: Do validation on date and convert to date object;
+        String date;
+        //TODO: Get day from Date object
+        String day;
+        //TODO: Convert time to date object, will be later used to save as reminder
+        String startTime;
+        String abstractText;
+        String description;
+        String venue;
+        String track;
+        String moderator;
+        JSONArray sessions = jsonData.getJSONArray("sessions");
+        for (int i = 0; i < sessions.length(); i++) {
+            try {
+                JSONObject jsonObject = sessions.getJSONObject(i);
+                id = i;
+                title = jsonObject.getString("title");
+                abstractText = jsonObject.getString("abstract");
+                subTitle = jsonObject.getString("type");
+
+                JSONObject dayObject = jsonObject.getJSONObject("day");
+                date = dayObject.getString("label_en");
+                startTime = "1:00 AM";
+                description = jsonObject.getString("description");
+                JSONObject venueObject = jsonObject.getJSONObject("location");
+                venue = venueObject.getString("label_en");
+                track = jsonObject.getJSONObject("track").getString("label_en");
+                moderator = "";
+                FossasiaEvent event = new FossasiaEvent(i, title, subTitle, date, "", startTime, abstractText, description, venue, track, moderator);
+
+                String query = "INSERT INTO %s VALUES ('%s', '%s', '%s');";
+
+                JSONArray jArray = jsonObject.getJSONArray("speakers");
+                for (int j = 0; j < jArray.length(); j++) {
+                    JSONObject jObj = jArray.getJSONObject(j);
+                    String fullName = jObj.getString("name");
+                    String speakerId = jObj.getString("id");
+                    String speakerQuery = String.format(query, DatabaseHelper.TABLE_NAME_SPEAKER_EVENT_RELATION, fullName, speakerId, StringUtils.replaceUnicode(title));
+                    queries.add(speakerQuery);
+
+                }
+
+                queries.add(event.generateSqlQuery());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void parseSpeakers(JSONObject jsonData) throws JSONException{
+
+        JSONArray speakers = jsonData.getJSONArray("speakers");
+        long id;
+        String name;
+        String information;
+        String linkedInUrl;
+        String twitterHandle;
+        String designation;
+        String profilePicUrl;
+        String organization;
+        boolean isKeySpeaker;
+
+        for (int i = 0; i < speakers.length(); i++) {
+            try {
+                JSONObject jsonObject = speakers.getJSONObject(i);
+                id = i;
+                name = jsonObject.getString("name");
+                information = jsonObject.getString("biography");
+                designation = jsonObject.getString("position");
+                organization = jsonObject.getString("organization");
+
+                if (organization != null && !organization.equals("")) {
+                    designation += ", " + organization;
+                }
+                profilePicUrl = jsonObject.getString("photo");
+                Speaker speaker = new Speaker(i, name, information, "", "", designation, profilePicUrl, 0);
+                queries.add(speaker.generateSqlQuery());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void parseTracks(JSONObject jsonData) throws JSONException{
+        JSONArray tracks = jsonData.getJSONArray("tracks");
+        for (int i = 0; i < tracks.length(); i++) {
+            String trackName = tracks.getJSONObject(i).getString("label_en");
+
+            String query = "INSERT INTO %s VALUES (%d, '%s', '%s');";
+            query = String.format(query, DatabaseHelper.TABLE_NAME_TRACK, i, StringUtils.replaceUnicode(trackName), "");
+            queries.add(query);
+        }
     }
 
 
