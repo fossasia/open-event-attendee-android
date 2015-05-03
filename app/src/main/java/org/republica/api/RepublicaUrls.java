5f4520a80bc2ba5dcf9d899d2d8e8eb6c4be61fe
Rafał <rafal.arte@gmail.com>
2015-05-03 20:17:26 +0200

    JsonToDatabase Refactored

--- app/src/main/java/org/republica/api/RepublicaUrls.java
+++ app/src/main/java/org/republica/api/RepublicaUrls.java
@@ -5,22 +5,6 @@ package org.republica.api;
  */
 public class RepublicaUrls {
 
-    public static final String SESSIONS_URL = "http://data.re-publica.de/data/rp15/data.json";
-
-    public static final String SPEAKERS_URL = "http://data.re-publica.de/api/rp15/speakers";
-
-    public static final String TRACKS_URL = "http://data.re-publica.de/api/rp15/tracks";
-
-    public static final String LOCATIONS_URL = "http://data.re-publica.de/api/rp15/locations";
-
-    public static final String MAPS_URL = "http://data.re-publica.de/api/rp15/maps";
-
-    public static final String POIS_URL = "http://data.re-publica.de/api/rp15/pois";
-
-    public static final String FORMATS_URL = "http://data.re-publica.de/api/rp15/formats";
-
-    public static final String LEVELS_URL = "http://data.re-publica.de/api/rp15/levels";
-
-    public static final String LANGUAGES_URL = "http://data.re-publica.de/api/rp15/languages";
+    public static final String DATA_URL = "http://data.re-publica.de/data/rp15/data.json";
 
 }
