0f4e7a7c8db7ada9807a6338cd90c9fd92345bb4
Arnav Gupta <championswimmer@gmail.com>
2015-05-06 04:10:43 +0530

    add event internal maps (of berlin station)
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- app/src/main/java/org/republica/fragments/MapFragment.java
+++ app/src/main/java/org/republica/fragments/MapFragment.java
@@ -1,13 +1,19 @@
 package org.republica.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.internal.view.menu.MenuBuilder;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -15,15 +21,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.republica.R;
+import org.republica.widgets.TouchImageView;
 
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
 import java.util.Locale;
 
-public class MapFragment extends SupportMapFragment {
+public class MapFragment extends Fragment {
 
     private static final double DESTINATION_LATITUDE = 52.50003;
     private static final double DESTINATION_LONGITUDE = 13.37560;
     private static final String DESTINATION_NAME = "STATION-Berlin";
     private GoogleMap mMap;
+    TouchImageView mapImageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,20 +44,23 @@ public class MapFragment extends SupportMapFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mMap = getMap();
-        if(mMap != null) {
-            mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-        }
+
     }
 
-    //    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//
-//        return inflater.inflate(R.layout.fragment_map, container, false);
-//    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mapImageView = (TouchImageView) rootView.findViewById(R.id.map_image_view);
+        mapImageView.setImageResource(R.drawable.eventmap_tues_wed);
+
+        return rootView;
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
         inflater.inflate(R.menu.map, menu);
     }
 
@@ -57,8 +70,40 @@ public class MapFragment extends SupportMapFragment {
             case R.id.directions:
                 launchDirections();
                 return true;
+            case R.id.day_select:
+                selectDay();
+                return true;
         }
         return false;
+    }
+
+    private void selectDay() {
+        AlertDialog.Builder b = new AlertDialog.Builder(getActivity());
+        b.setTitle(getResources().getString(R.string.select_map));
+        DateFormatSymbols dfs = new DateFormatSymbols(getResources().getConfiguration().locale);
+        String[] types = {dfs.getWeekdays()[3], dfs.getWeekdays()[4], dfs.getWeekdays()[5]};
+        b.setItems(types, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                switch (which) {
+                    case 0:
+                        mapImageView.setImageResource(R.drawable.eventmap_tues_wed);
+                        break;
+                    case 1:
+                        mapImageView.setImageResource(R.drawable.eventmap_tues_wed);
+                        break;
+                    case 2:
+                        mapImageView.setImageResource(R.drawable.eventmap_thurs);
+                        break;
+                }
+            }
+
+        });
+
+        b.show();
+
     }
 
     private void launchDirections() {
