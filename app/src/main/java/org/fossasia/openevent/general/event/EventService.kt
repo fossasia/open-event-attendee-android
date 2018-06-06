6ff2f942ac88f89e046c8980c137b84fa454ebb7
nikit19 <nikit.bhandari@gmail.com>
2018-06-06 23:06:11 +0530

    remove mapping

--- app/src/main/java/org/fossasia/openevent/general/event/EventService.kt
+++ app/src/main/java/org/fossasia/openevent/general/event/EventService.kt
@@ -5,8 +5,6 @@ import io.reactivex.Single
 
 class EventService(private val eventApi: EventApi, private val eventDao: EventDao) {
 
-    lateinit var searchEvents: List<Event>
-
     fun getEvents(): Flowable<List<Event>> {
         val eventsFlowable = eventDao.getAllEvents()
         return eventsFlowable.switchMap {
@@ -26,10 +24,7 @@ class EventService(private val eventApi: EventApi, private val eventDao: EventDa
 
     fun getSearchEvents(eventName: String): Single<List<Event>> {
         return eventApi.searchEvents("name", eventName)
-                .map {
-                    searchEvents = it
-                    it
-                }
+
     }
 
     fun getEvent(id: Long): Flowable<Event> {
