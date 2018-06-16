fac7fb4d4b74cf8ad89a8bef29465e09e531c033
Harsimar <simarsingh24@gmail.com>
2018-06-17 02:54:51 +0530

    feat: Add tickets caching (#114)
    * added : tickets caching
    
    minor changes
    
    * minor changes
    
    * minor changes
    
    * minor change
    minor change

--- app/src/main/java/org/fossasia/openevent/general/OpenEventDatabase.kt
+++ app/src/main/java/org/fossasia/openevent/general/OpenEventDatabase.kt
@@ -10,14 +10,18 @@ import org.fossasia.openevent.general.event.EventDao
 import org.fossasia.openevent.general.event.EventIdConverter
 import org.fossasia.openevent.general.social.SocialLink
 import org.fossasia.openevent.general.social.SocialLinksDao
+import org.fossasia.openevent.general.ticket.Ticket
+import org.fossasia.openevent.general.ticket.TicketsDao
 
-@Database(entities = [Event::class, User::class, SocialLink::class], version = 1)
+@Database(entities = [Event::class, User::class, SocialLink::class, Ticket::class], version = 1)
 @TypeConverters(EventIdConverter::class)
 abstract class OpenEventDatabase : RoomDatabase() {
 
     abstract fun eventDao(): EventDao
 
     abstract fun userDao(): UserDao
+
+    abstract fun ticketsDao(): TicketsDao
 
     abstract fun socialLinksDao(): SocialLinksDao
 }
