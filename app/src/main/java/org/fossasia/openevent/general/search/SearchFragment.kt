6df2b6b6394f8f32b16d42d2f0ea231a42813308
Harsimar <simarsingh24@gmail.com>
2018-07-03 22:58:10 +0530

    minor changes

--- app/src/main/java/org/fossasia/openevent/general/search/SearchFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/search/SearchFragment.kt
@@ -55,7 +55,6 @@ class SearchFragment : Fragment() {
                 eventsRecyclerAdapter.notifyItemChanged(id)
             }
         }
-
         eventsRecyclerAdapter.setFavorite(favouriteFabClickListener)
         eventsRecyclerAdapter.setListener(recyclerViewClickListener)
         searchViewModel.events.observe(this, Observer {
