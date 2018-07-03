c82e4e277bc72a8ef287e77abdda22efd4263019
Nikit Bhandari <nikit.bhandari@gmail.com>
2018-07-03 20:19:26 +0530

    fix: Format date and time correctly everywhere (#241)
    * fix: format date and time correctly everywhere
    
    * add tests
    
    * move logic to util
    
    * tests updated
    
    * update util method and test
    
    * minor changes
    
    * indentation

--- app/src/test/java/org/fossasia/openevent/general/event/EventUtilsTest.kt
+++ app/src/test/java/org/fossasia/openevent/general/event/EventUtilsTest.kt
@@ -9,6 +9,7 @@ import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import org.threeten.bp.ZonedDateTime
 import java.time.ZoneId
 import java.util.*
 
@@ -42,6 +43,8 @@ class EventUtilsTest {
                          link: String? = null) =
             Event(id, name, identifier, startsAt, endsAt, timeZone,
                     description = description, externalEventUrl = link)
+
+    private fun getLocalizedDate(dateTime: String): ZonedDateTime = EventUtils.getLocalizedDateTime(dateTime)
 
     private fun setupStringMock() {
         every { resource.getString(R.string.event_name) }.returns("Event Name : ")
@@ -107,6 +110,111 @@ class EventUtilsTest {
             Ends On : 19 Sep 2008 07:55 PM
             Event Link : https://open-event-frontend-dev.herokuapp.com/e/abcdefgh
             """.trimIndent(), EventUtils.getSharableInfo(event, resource))
+    }
+
+    @Test
+    fun `should get timezone name`() {
+        val event = getEvent()
+        val localizedDateTime = getLocalizedDate(event.startsAt)
+        assertEquals("""
+           IST
+            """.trimIndent(), EventUtils.getFormattedTimeZone(localizedDateTime))
+    }
+
+    @Test
+    fun `should get formatted time`() {
+        val event = getEvent()
+        val localizedDateTime = getLocalizedDate(event.startsAt)
+        assertEquals("""
+           04:23 PM
+            """.trimIndent(), EventUtils.getFormattedTime(localizedDateTime))
+    }
+
+    @Test
+    fun `should get formatted date and time without year`() {
+        val event = getEvent()
+        val localizedDateTime = getLocalizedDate(event.startsAt)
+        assertEquals("""
+          Monday, Sep 15
+            """.trimIndent(), EventUtils.getFormattedDateWithoutYear(localizedDateTime))
+    }
+
+    @Test
+    fun `should get formatted date short`() {
+        val event = getEvent()
+        val localizedDateTime = getLocalizedDate(event.startsAt)
+        assertEquals("""
+          Mon, Sep 15
+            """.trimIndent(), EventUtils.getFormattedDateShort(localizedDateTime))
+    }
+
+    @Test
+    fun `should get formatted date`() {
+        val event = getEvent()
+        val localizedDateTime = getLocalizedDate(event.startsAt)
+        assertEquals("""
+          Monday, Sep 15, 2008
+            """.trimIndent(), EventUtils.getFormattedDate(localizedDateTime))
+    }
+
+    @Test
+    fun `should get formatted date range when start and end date are not same`() {
+        val event = getEvent()
+        val startsAt = getLocalizedDate(event.startsAt)
+        val endsAt = getLocalizedDate(event.endsAt)
+        assertEquals("""
+         Mon, Sep 15, 04:23 PM
+            """.trimIndent(), EventUtils.getFormattedEventDateTimeRange(startsAt, endsAt))
+    }
+
+    @Test
+    fun `should get formatted date range when start and end date are same`() {
+        val event = getEvent(endsAt = "2008-09-15T15:53:00+05:00")
+        val startsAt = getLocalizedDate(event.startsAt)
+        val endsAt = getLocalizedDate(event.endsAt)
+        assertEquals("""
+          Monday, Sep 15
+            """.trimIndent(), EventUtils.getFormattedEventDateTimeRange(startsAt, endsAt))
+    }
+
+    @Test
+    fun `should get formatted date range when start and end date are not same in event details`() {
+        val event = getEvent()
+        val startsAt = getLocalizedDate(event.startsAt)
+        val endsAt = getLocalizedDate(event.endsAt)
+        assertEquals("""
+          - Fri, Sep 19, 07:55 PM IST
+            """.trimIndent(), EventUtils.getFormattedEventDateTimeRangeSecond(startsAt, endsAt))
+    }
+
+    @Test
+    fun `should get formatted date range when start and end date are same in event details`() {
+        val event = getEvent(endsAt = "2008-09-15T15:53:00+05:00")
+        val startsAt = getLocalizedDate(event.startsAt)
+        val endsAt = getLocalizedDate(event.endsAt)
+        assertEquals("""
+          04:23 PM - 04:23 PM IST
+            """.trimIndent(), EventUtils.getFormattedEventDateTimeRangeSecond(startsAt, endsAt))
+    }
+
+    @Test
+    fun `should get formatted date range when start and end date are not same in details`() {
+        val event = getEvent()
+        val startsAt = getLocalizedDate(event.startsAt)
+        val endsAt = getLocalizedDate(event.endsAt)
+        assertEquals("""
+          Monday, Sep 15, 2008 at 04:23 PM - Friday, Sep 19, 2008 at 07:55 PM (IST)
+            """.trimIndent(), EventUtils.getFormattedDateTimeRangeDetailed(startsAt, endsAt))
+    }
+
+    @Test
+    fun `should get formatted date range when start and end date are same in details`() {
+        val event = getEvent(endsAt = "2008-09-15T15:53:00+05:00")
+        val startsAt = getLocalizedDate(event.startsAt)
+        val endsAt = getLocalizedDate(event.endsAt)
+        assertEquals("""
+          Monday, Sep 15, 2008 from 04:23 PM to 04:23 PM (IST)
+            """.trimIndent(), EventUtils.getFormattedDateTimeRangeDetailed(startsAt, endsAt))
     }
 
 }
