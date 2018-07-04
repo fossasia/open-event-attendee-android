089e5413d10cb17f170042cb8bdc960c50455bba
Sanyog Ghosh <f20160635@goa.bits-pilani.ac.in>
2018-07-04 19:00:49 +0530

    feat: Exclude same event from Similar Events

--- app/src/main/java/org/fossasia/openevent/general/event/EventsRecyclerAdapter.kt
+++ app/src/main/java/org/fossasia/openevent/general/event/EventsRecyclerAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.fossasia.openevent.general.R
-import org.fossasia.openevent.general.event.topic.SimilarEventsFragment
 import java.util.*
 
 class EventsRecyclerAdapter : RecyclerView.Adapter<EventViewHolder>() {
@@ -13,6 +12,8 @@ class EventsRecyclerAdapter : RecyclerView.Adapter<EventViewHolder>() {
     private var clickListener: RecyclerViewClickListener? = null
     private var favoriteFabListener: FavoriteFabListener? = null
     private var eventLayout: String? = null
+    private var idEvent: Long = -1
+    private lateinit var eventView: View
 
     fun setListener(listener: RecyclerViewClickListener) {
         clickListener = listener
@@ -26,6 +27,10 @@ class EventsRecyclerAdapter : RecyclerView.Adapter<EventViewHolder>() {
         eventLayout = type
     }
 
+    fun setEventId(id: Long) {
+        idEvent = id
+    }
+
     fun addAll(eventList: List<Event>) {
         if (events.isNotEmpty())
             this.events.clear()
@@ -33,7 +38,6 @@ class EventsRecyclerAdapter : RecyclerView.Adapter<EventViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
-        val eventView: View
         if (eventLayout.equals(SIMILAR_EVENTS)) {
             eventView = LayoutInflater.from(parent.context).inflate(R.layout.item_card_similar_events, parent, false)
         } else {
@@ -44,13 +48,25 @@ class EventsRecyclerAdapter : RecyclerView.Adapter<EventViewHolder>() {
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         val event = events[position]
-        holder.bind(event, clickListener, favoriteFabListener)
+        handleEventVisibility(event, holder)
     }
 
     override fun getItemCount(): Int {
         return events.size
     }
 
+    private fun handleEventVisibility(event: Event, holder: EventViewHolder) {
+        if (eventLayout.equals(SIMILAR_EVENTS)) {
+            if (idEvent == event.id) {
+                eventView.layoutParams = RecyclerView.LayoutParams(0, 0)
+                eventView.visibility = View.GONE
+            } else {
+                holder.bind(event, clickListener, favoriteFabListener)
+            }
+        } else {
+            holder.bind(event, clickListener, favoriteFabListener)
+        }
+    }
 }
 
 interface RecyclerViewClickListener {
