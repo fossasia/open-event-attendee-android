fb3c76b018c58502464bb8032e32266b774f64f1
Sanyog Ghosh <f20160635@goa.bits-pilani.ac.in>
2018-07-05 16:31:00 +0530

    fix: used switch map
    fix: used switch map

--- app/src/main/java/org/fossasia/openevent/general/event/topic/SimilarEventsViewModel.kt
+++ app/src/main/java/org/fossasia/openevent/general/event/topic/SimilarEventsViewModel.kt
@@ -2,13 +2,13 @@ package org.fossasia.openevent.general.event.topic
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import org.fossasia.openevent.general.event.Event
 import org.fossasia.openevent.general.event.EventService
 import timber.log.Timber
-import kotlin.collections.ArrayList
 
 class SimilarEventsViewModel(private val eventService: EventService) : ViewModel() {
 
@@ -17,7 +17,6 @@ class SimilarEventsViewModel(private val eventService: EventService) : ViewModel
     val progress = MutableLiveData<Boolean>()
     val similarEvents = MutableLiveData<List<Event>>()
     val error = MutableLiveData<String>()
-    var eventList = ArrayList<Event>()
     var eventId: Long = -1
 
     fun loadSimilarEvents(id: Long) {
@@ -26,15 +25,18 @@ class SimilarEventsViewModel(private val eventService: EventService) : ViewModel
         }
         compositeDisposable.add(eventService.getSimilarEvents(id)
                 .subscribeOn(Schedulers.io())
-                .flatMapIterable{ it }
-                .filter { it.id != eventId }
+                .switchMap {
+                    Observable.fromIterable(it)
+                        .filter{ it.id != eventId }
+                        .toList()
+                        .toFlowable()
+                }
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnSubscribe({
                     progress.value = true
                 }).subscribe({
                     progress.value = false
-                    eventList.add(it)
-                    similarEvents.value = eventList
+                    similarEvents.value = it
                 }, {
                     Timber.e(it, "Error fetching similar events")
                     error.value = "Error fetching similar events"
