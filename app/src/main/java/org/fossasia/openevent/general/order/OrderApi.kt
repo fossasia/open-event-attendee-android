95b14c524ff69d98b22adf6afc3b6bc41688b303
nikit19 <nikit.bhandari@gmail.com>
2018-07-12 16:51:02 +0530

    feat: add charge model and API

--- app/src/main/java/org/fossasia/openevent/general/order/OrderApi.kt
+++ app/src/main/java/org/fossasia/openevent/general/order/OrderApi.kt
@@ -3,10 +3,14 @@ package org.fossasia.openevent.general.order
 import io.reactivex.Single
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface OrderApi {
 
     @POST("orders")
     fun placeOrder(@Body order: Order): Single<Order>
+
+    @POST("orders/{orderIdentifier}/charge")
+    fun chargeOrder(@Path("orderIdentifier") orderIdentifier: String, @Body charge: Charge): Single<Charge>
 
 }
