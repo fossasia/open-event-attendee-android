c2987559eb3ff3f191affd41ef556c60e16cb8f7
nikit19 <nikit.bhandari@gmail.com>
2018-07-23 02:29:25 +0530

    feat: add order completed screen

---
+++ app/src/main/java/org/fossasia/openevent/general/order/OrderCompletedFragment.kt
@@ -0,0 +1,140 @@
+package org.fossasia.openevent.general.order
+
+import android.arch.lifecycle.Observer
+import android.content.Intent
+import android.os.Bundle
+import android.provider.CalendarContract
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v7.app.AppCompatActivity
+import android.view.*
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_order.view.*
+import org.fossasia.openevent.general.MainActivity
+import org.fossasia.openevent.general.R
+import org.fossasia.openevent.general.event.Event
+import org.fossasia.openevent.general.event.EventDetailsFragment
+import org.fossasia.openevent.general.event.EventUtils
+import org.fossasia.openevent.general.ticket.EVENT_ID
+import org.koin.android.architecture.ext.viewModel
+
+class OrderCompletedFragment : Fragment() {
+
+    private lateinit var rootView: View
+    private lateinit var eventShare: Event
+    private var id: Long = -1
+    private val orderCompletedViewModel by viewModel<OrderCompletedViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val bundle = this.arguments
+        if (bundle != null) {
+            id = bundle.getLong(EVENT_ID, -1)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        rootView = inflater.inflate(R.layout.fragment_order, container, false)
+        val activity = activity as? AppCompatActivity
+        activity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        activity?.supportActionBar?.title = ""
+        setHasOptionsMenu(true)
+
+        orderCompletedViewModel.loadEvent(id)
+        orderCompletedViewModel.event.observe(this, Observer {
+            it?.let {
+                loadEventDetails(it)
+                eventShare = it
+            }
+        })
+
+        orderCompletedViewModel.message.observe(this, Observer {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+        })
+
+        rootView.add.setOnClickListener {
+            startCalendar(eventShare)
+        }
+
+        rootView.share.setOnClickListener {
+            shareEvent(eventShare)
+        }
+
+        return rootView
+    }
+
+    private fun loadEventDetails(event: Event) {
+        val dateString = StringBuilder()
+        val startsAt = EventUtils.getLocalizedDateTime(event.startsAt)
+
+        rootView.name.text = "${event.name}"
+        rootView.time.text = dateString.append(EventUtils.getFormattedDateShort(startsAt))
+                .append(" • ")
+                .append(EventUtils.getFormattedTime(startsAt))
+                .append(" ")
+                .append(EventUtils.getFormattedTimeZone(startsAt))
+
+    }
+
+    private fun startCalendar(event: Event) {
+        val intent = Intent(Intent.ACTION_INSERT)
+        intent.type = "vnd.android.cursor.item/event"
+        intent.putExtra(CalendarContract.Events.TITLE, event.name)
+        intent.putExtra(CalendarContract.Events.DESCRIPTION, event.description)
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, EventUtils.getTimeInMilliSeconds(event.startsAt))
+        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, EventUtils.getTimeInMilliSeconds(event.endsAt))
+        startActivity(intent)
+    }
+
+    private fun shareEvent(event: Event) {
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(Intent.EXTRA_TEXT, EventUtils.getSharableInfo(event))
+        sendIntent.type = "text/plain"
+        startActivity(Intent.createChooser(sendIntent, "Share Event Details"))
+    }
+
+    private fun redirectToMain() {
+        activity?.supportFragmentManager?.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        val intent = Intent(activity, MainActivity::class.java)
+        startActivity(intent)
+        activity?.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        activity?.finish()
+    }
+
+    private fun openEventDetails() {
+        val eventDetailsFragment = EventDetailsFragment()
+        val bundle = Bundle()
+        bundle.putLong("EVENT_ID", id)
+        eventDetailsFragment.arguments = bundle
+        activity?.supportFragmentManager?.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.rootLayout, eventDetailsFragment)?.addToBackStack(null)?.commit()
+    }
+
+    override fun onDestroyView() {
+        val activity = activity as? MainActivity
+        activity?.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        setHasOptionsMenu(false)
+        super.onDestroyView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        val inflaterMenu = activity?.menuInflater
+        inflaterMenu?.inflate(R.menu.order_completed, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                openEventDetails()
+                true
+            }
+            R.id.tick -> {
+                redirectToMain()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
