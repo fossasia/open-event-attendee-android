69106583a69d8b6a3ad76445b6defc2599b9959c
nikit19 <nikit.bhandari@gmail.com>
2018-07-31 19:14:01 +0530

    fix: show event info in my tickets

--- app/src/main/java/org/fossasia/openevent/general/order/OrdersRecyclerAdapter.kt
+++ app/src/main/java/org/fossasia/openevent/general/order/OrdersRecyclerAdapter.kt
@@ -4,21 +4,27 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import org.fossasia.openevent.general.R
+import org.fossasia.openevent.general.event.Event
 import kotlin.collections.ArrayList
 
 class OrdersRecyclerAdapter : RecyclerView.Adapter<OrdersViewHolder>() {
 
-    private val orders = ArrayList<Order>()
+    private val events = ArrayList<Event>()
     private var clickListener: OrderClickListener? = null
+    private var orderIdentifier: String? = null
 
     fun setListener(listener: OrderClickListener) {
         clickListener = listener
     }
 
-    fun addAll(orderList: List<Order>) {
-        if (orders.isNotEmpty())
-            this.orders.clear()
-        this.orders.addAll(orderList)
+    fun addAll(orderList: List<Event>) {
+        if (events.isNotEmpty())
+            this.events.clear()
+        this.events.addAll(orderList)
+    }
+
+    fun setOrderIdentifier(orderId: String?) {
+        orderIdentifier = orderId
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrdersViewHolder {
@@ -27,12 +33,12 @@ class OrdersRecyclerAdapter : RecyclerView.Adapter<OrdersViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: OrdersViewHolder, position: Int) {
-        val order = orders[position]
-        holder.bind(order, clickListener)
+        val event = events[position]
+        holder.bind(event, clickListener, orderIdentifier)
     }
 
     override fun getItemCount(): Int {
-        return orders.size
+        return events.size
     }
 
     interface OrderClickListener {
