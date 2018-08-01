ebbf96c661c9403ef120b492cfa5b884acd218e5
Sanyog Ghosh <f20160635@goa.bits-pilani.ac.in>
2018-08-02 01:32:58 +0530

    feat: Add custom form APi and model

--- app/src/main/java/org/fossasia/openevent/general/attendees/AttendeeService.kt
+++ app/src/main/java/org/fossasia/openevent/general/attendees/AttendeeService.kt
@@ -2,6 +2,7 @@ package org.fossasia.openevent.general.attendees
 
 import io.reactivex.Completable
 import io.reactivex.Single
+import org.fossasia.openevent.general.attendees.forms.CustomForm
 import org.fossasia.openevent.general.auth.User
 import org.fossasia.openevent.general.auth.UserDao
 
@@ -20,5 +21,9 @@ class AttendeeService(private val attendeeApi: AttendeeApi, private val attendee
 
     fun deleteAttendee(id: Long): Completable {
         return attendeeApi.deleteAttendee(id)
+    }
+
+    fun getCustomFormsForAttendees(id: Long): Single<List<CustomForm>> {
+        return attendeeApi.getCustomFormsForAttendees(id)
     }
 }
