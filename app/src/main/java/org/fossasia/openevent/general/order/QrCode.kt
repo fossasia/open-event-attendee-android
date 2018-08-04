35719e8a8c022d4b852edc4ead4d6d9362981096
Harsimar <simarsingh24@gmail.com>
2018-08-04 12:17:07 +0530

    feat: added QR code in order detail fragment (#418)
    * feat: added QR code in order detail fragment
    
    
    minor changes
    
    
    minor changes
    
    
    minor changes
    
    
    minor changes
    
    
    minor changes
    
    
    minor changes
    
    
    minor changes
    
    * minor changes

---
+++ app/src/main/java/org/fossasia/openevent/general/order/QrCode.kt
@@ -0,0 +1,24 @@
+package org.fossasia.openevent.general.order
+
+import android.graphics.Bitmap
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import timber.log.Timber
+
+class QrCode {
+    private val multiFormatWriter = MultiFormatWriter()
+    private val barcodeEncoder = BarcodeEncoder()
+
+    fun generateQrBitmap(text: String?, width: Int, height: Int): Bitmap? {
+        try {
+            val bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, width, height)
+            return barcodeEncoder.createBitmap(bitMatrix)
+        } catch (e: WriterException) {
+            Timber.d(e, "Writer Exception")
+        }
+        return null
+    }
+
+}
