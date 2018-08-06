41bfb8909d3a45729a7d6b3d5c2c341cf54c3ad6
nikit19 <nikit.bhandari@gmail.com>
2018-08-06 18:58:50 +0530

    chore: remove oss library from the app

--- app/src/playStore/java/org/fossasia/openevent/general/settings/AcknowledgementDecider.kt
+++ app/src/playStore/java/org/fossasia/openevent/general/settings/AcknowledgementDecider.kt
@@ -1,17 +1,14 @@
 package org.fossasia.openevent.general.settings
 
 import android.content.Context
-import android.content.Intent
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 object AcknowledgementDecider {
 
     fun decide(context: Context) {
-        val intent = Intent(context, OssLicensesMenuActivity::class.java)
-        context.startActivity(intent)
+        //oss library has been removed from the playstore build as well
     }
 
     fun showAcknowledgement(): Boolean {
-        return true
+        return false
     }
 }
