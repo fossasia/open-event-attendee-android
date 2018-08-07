71c126c4dfa5c808fbc0e5ce75f9f6975f760565
Harsimar <simarsingh24@gmail.com>
2018-08-07 16:45:19 +0530

    feat: create multiple attendees with different details
    minor changes
    
    
    minor changes

--- app/src/main/java/org/fossasia/openevent/general/attendees/AttendeeViewHolder.kt
+++ app/src/main/java/org/fossasia/openevent/general/attendees/AttendeeViewHolder.kt
@@ -2,21 +2,46 @@ package org.fossasia.openevent.general.attendees
 
 import android.support.design.widget.TextInputLayout
 import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import kotlinx.android.synthetic.main.item_attendee.view.*
 import org.fossasia.openevent.general.attendees.forms.CustomForm
-import org.fossasia.openevent.general.ticket.Ticket
+import org.fossasia.openevent.general.ticket.TicketId
 
 class AttendeeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(attendeesAndTicket: Pair<Attendee, Ticket>, forms: List<CustomForm>, formVisibility: Boolean) {
-        setText(itemView.attendeeItemCountry, attendeesAndTicket.first.country)
-        setText(itemView.attendeeItemLastName, attendeesAndTicket.first.lastname)
-        setText(itemView.attendeeItemEmail, attendeesAndTicket.first.email)
-        setText(itemView.attendeeItemFirstName, attendeesAndTicket.first.firstname)
-        itemView.attendeeItemTicketName.text = attendeesAndTicket.second.name
+    fun bind(attendeeRecyclerAdapter: AttendeeRecyclerAdapter, forms: List<CustomForm>, formVisibility: Boolean, position: Int) {
+        setText(itemView.attendeeItemCountry, attendeeRecyclerAdapter.attendeeList[position].country)
+        setText(itemView.attendeeItemLastName, attendeeRecyclerAdapter.attendeeList[position].lastname)
+        setText(itemView.attendeeItemEmail, attendeeRecyclerAdapter.attendeeList[position].email)
+        setText(itemView.attendeeItemFirstName, attendeeRecyclerAdapter.attendeeList[position].firstname)
+        itemView.attendeeItemTicketName.text = attendeeRecyclerAdapter.ticketList[position].name
         fillInformationSection(forms, formVisibility)
+        val textWatcher = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val id = attendeeRecyclerAdapter.attendeeList[position].id
+                attendeeRecyclerAdapter.attendeeList.removeAt(position)
+                val attendee = Attendee(id, firstname = itemView.attendeeItemFirstName.text.toString(),
+                        lastname = itemView.attendeeItemLastName.text.toString(),
+                        email = itemView.attendeeItemEmail.text.toString(),
+                        country = itemView.attendeeItemCountry.text.toString(),
+                        ticket = TicketId(attendeeRecyclerAdapter.ticketList[position].id.toLong()),
+                        event = attendeeRecyclerAdapter.eventId)
+                attendeeRecyclerAdapter.attendeeList.add(position, attendee)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        }
+        itemView.attendeeItemFirstName.addTextChangedListener(textWatcher)
+        itemView.attendeeItemLastName.addTextChangedListener(textWatcher)
+        itemView.attendeeItemEmail.addTextChangedListener(textWatcher)
+        itemView.attendeeItemCountry.addTextChangedListener(textWatcher)
     }
 
     fun setText(editText: EditText, string: String?) {
