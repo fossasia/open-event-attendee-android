8b80917a35034e8d802b3a1d23ff19f3ea450a04
Harsimar <simarsingh24@gmail.com>
2018-08-13 14:31:43 +0530

    fix: duplicate APK's bug in APK branch
    minor changes

--- scripts/update-apk.sh
+++ scripts/update-apk.sh
@@ -34,7 +34,7 @@ if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
 		if [[ $file = "open-event"* ]]; then
 			continue
 		fi
-		cp $file open-event-master-${file%%}
+		mv $file open-event-master-${file%%}
 	done
 fi
 
@@ -43,7 +43,7 @@ if [ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" ]; then
 		if [[ $file = "open-event"* ]]; then
 			continue
 		fi
-		cp $file open-event-dev-${file%%}
+		mv $file open-event-dev-${file%%}
 	done
 fi
 
@@ -51,7 +51,7 @@ fi
 
 if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     echo "Push to master branch detected, signing the app..."
-    cp app-release-unsigned.apk app-release-unaligned.apk
+    cp open-event-master-app-playStore-release-unsigned.apk app-release-unaligned.apk
 	jarsigner -verbose -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../scripts/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
 	${ANDROID_HOME}/build-tools/27.0.3/zipalign -v -p 4 app-release-unaligned.apk app-release.apk
 fi
