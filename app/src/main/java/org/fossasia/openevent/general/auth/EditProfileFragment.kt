f489b46e0e48125c80c7c3caf3c9a934eb45334c
Shivansh Beohar <33032175+trancenoid@users.noreply.github.com>
2018-11-05 21:07:59 +0530

    fix: Check image URL to be empty before loading (#684)

--- app/src/main/java/org/fossasia/openevent/general/auth/EditProfileFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/auth/EditProfileFragment.kt
@@ -55,14 +55,16 @@ class EditProfileFragment : Fragment() {
                 val imageUrl = it.avatarUrl.nullToEmpty()
                 rootView.firstName.setText(userFirstName)
                 rootView.lastName.setText(userLastName)
-                context?.let { ctx ->
-                    val drawable = AppCompatResources.getDrawable(ctx, R.drawable.ic_account_circle_grey_24dp)
-                    drawable?.let { icon ->
-                        Picasso.get()
-                            .load(imageUrl)
-                            .placeholder(icon)
-                            .transform(CircleTransform())
-                            .into(rootView.profilePhoto)
+                if (!imageUrl.isEmpty()) { // picasso requires the imageUrl to be non empty
+                    context?.let { ctx ->
+                        val drawable = AppCompatResources.getDrawable(ctx, R.drawable.ic_account_circle_grey_24dp)
+                        drawable?.let { icon ->
+                            Picasso.get()
+                                    .load(imageUrl)
+                                    .placeholder(icon)
+                                    .transform(CircleTransform())
+                                    .into(rootView.profilePhoto)
+                        }
                     }
                 }
             }
