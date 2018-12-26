4b9accb16d50f686d654683403521e62fd3af49e
Kshitij Suri <kshitijsuri90@gmail.com>
2018-12-26 22:45:20 +0530

    "Import conflicts"

--- app/src/fdroid/java/org/fossasia/openevent/general/search/GeoLocationUI.kt
+++ app/src/fdroid/java/org/fossasia/openevent/general/search/GeoLocationUI.kt
@@ -7,11 +7,8 @@ import kotlinx.android.synthetic.main.activity_search_location.*
 class GeoLocationUI {
 
     fun configure(activity: Activity, searchLocationViewModel: SearchLocationViewModel){
-        //Feature not available for F-droid
         activity.currentLocation.visibility = View.GONE
     }
-
     fun search(activity: Activity,fromSearchFragment: Boolean , searchLocationViewModel: SearchLocationViewModel, query:String){
-        //Feature not available for F-droid
     }
 }
