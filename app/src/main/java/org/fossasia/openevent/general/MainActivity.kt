407ceac5916bdba1db99a97e1309e7e59a367e98
liveHarshit <live.harshit@hotmail.com>
2019-01-03 22:42:26 +0530

    feat: Add navigation architecture for MainActivity host (#822) (#780) (#760) (#726) (#719)

--- app/src/main/java/org/fossasia/openevent/general/MainActivity.kt
+++ app/src/main/java/org/fossasia/openevent/general/MainActivity.kt
@@ -2,105 +2,39 @@ package org.fossasia.openevent.general
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.frameContainer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.navigation
-import org.fossasia.openevent.general.R.id.navigation_events
-import org.fossasia.openevent.general.R.id.navigation_search
-import org.fossasia.openevent.general.auth.ProfileFragment
-import org.fossasia.openevent.general.event.EventDetailsFragment
-import org.fossasia.openevent.general.event.EventsFragment
-import org.fossasia.openevent.general.favorite.FavoriteFragment
 import org.fossasia.openevent.general.order.LAUNCH_TICKETS
-import org.fossasia.openevent.general.order.OrdersUnderUserFragment
 import org.fossasia.openevent.general.order.TICKETS
-import org.fossasia.openevent.general.search.SearchFragment
-import org.fossasia.openevent.general.utils.Utils.checkAndLoadFragment
-import org.fossasia.openevent.general.utils.Utils.loadFragment
 
 private const val TO_SEARCH: String = "ToSearchFragment"
 
 class MainActivity : AppCompatActivity() {
 
-    private val listener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        val fragment: Fragment
-        when (item.itemId) {
-            R.id.navigation_events -> {
-                supportActionBar?.title = "Events"
-                fragment = EventsFragment()
-                checkAndLoadFragment(supportFragmentManager, fragment, frameContainer.id)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_search -> {
-                supportActionBar?.title = "Search"
-                fragment = SearchFragment()
-                checkAndLoadFragment(supportFragmentManager, fragment, frameContainer.id)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_profile -> {
-                supportActionBar?.title = "Profile"
-                fragment = ProfileFragment()
-                checkAndLoadFragment(supportFragmentManager, fragment, frameContainer.id)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_favorite -> {
-                supportActionBar?.title = "Likes"
-                fragment = FavoriteFragment()
-                checkAndLoadFragment(supportFragmentManager, fragment, frameContainer.id)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_tickets -> {
-                supportActionBar?.title = "Tickets"
-                fragment = OrdersUnderUserFragment()
-                checkAndLoadFragment(supportFragmentManager, fragment, frameContainer.id)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(listener)
+        val hostFragment = supportFragmentManager.findFragmentById(R.id.frameContainer)
+        var navController: NavController? = null
+        if (hostFragment is NavHostFragment) {
+            navController = hostFragment.navController
+            setupBottomNavigationMenu(navController)
+        }
 
         val bundle = if (savedInstanceState == null) intent.extras else null
         if (bundle != null) {
-            if (bundle.getBoolean(TO_SEARCH)) {
-                loadFragment(supportFragmentManager, SearchFragment(), frameContainer.id)
-                supportActionBar?.title = "Search"
-                navigation.selectedItemId = navigation_search
-            }
+            if (bundle.getBoolean(TO_SEARCH))
+                navController?.navigate(R.id.navigation_search)
 
-            if (bundle.getBoolean(TICKETS) || bundle.getBoolean(LAUNCH_TICKETS)) {
-                loadFragment(supportFragmentManager, OrdersUnderUserFragment(), frameContainer.id)
-                supportActionBar?.title = "Tickets"
-                navigation.selectedItemId = R.id.navigation_tickets
-            }
-        } else {
-            supportActionBar?.title = "Events"
-            loadFragment(supportFragmentManager, EventsFragment(), frameContainer.id)
+            if (bundle.getBoolean(TICKETS) || bundle.getBoolean(LAUNCH_TICKETS))
+                navController?.navigate(R.id.navigation_tickets)
         }
     }
 
-    override fun onBackPressed() {
-        val currentFragment = this.supportFragmentManager.findFragmentById(R.id.frameContainer)
-        val rootFragment = this.supportFragmentManager.findFragmentById(R.id.rootLayout)
-        if (rootFragment is EventDetailsFragment)
-            super.onBackPressed()
-        else
-            when (currentFragment) {
-                is SearchFragment,
-                is FavoriteFragment,
-                is OrdersUnderUserFragment,
-                is ProfileFragment -> {
-                    loadFragment(supportFragmentManager, EventsFragment(), frameContainer.id)
-                    navigation.selectedItemId = navigation_events
-                }
-                is EventsFragment -> finish()
-                else -> super.onBackPressed()
-            }
+    private fun setupBottomNavigationMenu(navController: NavController) {
+        setupWithNavController(navigation, navController)
     }
 }
