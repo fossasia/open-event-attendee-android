677d07606a87ba52d3ccfdbee3300ab629a8ec4c
Kshitij Suri <kshitijsuri90@gmail.com>
2019-01-23 19:42:10 +0530

    Used navigation controller

--- app/src/main/java/org/fossasia/openevent/general/WelcomeFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/WelcomeFragment.kt
@@ -1,7 +1,6 @@
 package org.fossasia.openevent.general
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
