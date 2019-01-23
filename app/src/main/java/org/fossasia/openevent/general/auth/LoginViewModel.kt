b24cedacfd93d2ebb1b33eeefc34466f7188ca11
Cool-fire <upendrareddy2511@gmail.com>
2019-01-23 20:30:19 +0530

    Added Textlayout error for email field

--- app/src/main/java/org/fossasia/openevent/general/auth/LoginViewModel.kt
+++ app/src/main/java/org/fossasia/openevent/general/auth/LoginViewModel.kt
@@ -24,6 +24,8 @@ class LoginViewModel(
     val user: LiveData<User> = mutableUser
     private val mutableError = MutableLiveData<String>()
     val error: LiveData<String> = mutableError
+    private val mutableEmailError = MutableLiveData<Boolean>()
+    val emailError: LiveData<Boolean> = mutableEmailError
     private val mutableShowNoInternetDialog = MutableLiveData<Boolean>()
     val showNoInternetDialog: LiveData<Boolean> = mutableShowNoInternetDialog
     private val mutableRequestTokenSuccess = MutableLiveData<Boolean>()
@@ -56,11 +58,10 @@ class LoginViewModel(
 
     private fun hasErrors(email: String?, password: String?): Boolean {
         if (email.isNullOrEmpty() || password.isNullOrEmpty()) {
-            mutableIsCorrectEmail.value = !email.isNullOrEmpty()
             mutableError.value = "Email or Password cannot be empty!"
             return true
         } else if (mutableIsCorrectEmail?.value == false) {
-            mutableError.value = "Please enter valid Email"
+            mutableEmailError.value = false
             return true
         }
         return false
