eeb3f607192542c88d5639e77694783d247d17b6
Ankur Ingale <33569473+AnkurIngale@users.noreply.github.com>
2019-02-02 16:58:34 +0530

    Issue #995

--- app/src/main/java/org/fossasia/openevent/general/attendees/AttendeeFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/attendees/AttendeeFragment.kt
@@ -274,7 +274,7 @@ class AttendeeFragment : Fragment() {
         attendeeViewModel.message
             .nonNull()
             .observe(this, Observer {
-                Snackbar.make(rootView.attendeeCoordinatorLayout, it, Snackbar.LENGTH_LONG).show()
+                Snackbar.make(rootView, it, Snackbar.LENGTH_LONG).show()
             })
 
         attendeeViewModel.progress
@@ -422,7 +422,7 @@ class AttendeeFragment : Fragment() {
         val validDetails: Boolean? = card.validateCard()
         if (validDetails != null && !validDetails)
             Snackbar.make(
-                rootView.attendeeCoordinatorLayout, "Invalid card data", Snackbar.LENGTH_SHORT
+                rootView, "Invalid card data", Snackbar.LENGTH_SHORT
             ).show()
         else
             Stripe(requireContext())
@@ -435,7 +435,7 @@ class AttendeeFragment : Fragment() {
 
                     override fun onError(error: Exception) {
                         Snackbar.make(
-                            rootView.attendeeCoordinatorLayout, error.localizedMessage.toString(), Snackbar.LENGTH_LONG
+                            rootView, error.localizedMessage.toString(), Snackbar.LENGTH_LONG
                         ).show()
                     }
                 })
