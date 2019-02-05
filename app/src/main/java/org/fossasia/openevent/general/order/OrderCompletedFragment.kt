0414fb9a8e1c06341f82f6f68053c3e25cb842f2
Anunay Rajhans <anunay99@gmail.com>
2019-02-05 16:04:19 +0530

    Removed unused import.

--- app/src/main/java/org/fossasia/openevent/general/order/OrderCompletedFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/order/OrderCompletedFragment.kt
@@ -9,7 +9,6 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
