35f84c939e3edce0b3eb46be598d40df588a7c6e
Anunay Rajhans <anunay99@gmail.com>
2019-02-05 16:54:59 +0530

    Fix #495 Replaced FAB with ImageButton and removed elevation.

--- app/src/main/java/org/fossasia/openevent/general/order/OrderCompletedFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/order/OrderCompletedFragment.kt
@@ -9,7 +9,6 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -84,8 +83,6 @@ class OrderCompletedFragment : Fragment() {
         rootView.share.setOnClickListener {
             shareEvent(eventShare)
         }
-
-        rootView.share.scaleType = ImageView.ScaleType.CENTER
 
         return rootView
     }
