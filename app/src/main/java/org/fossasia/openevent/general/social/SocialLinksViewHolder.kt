731742c9be5b48c6806a7773701a99ae4c27df8a
Anunay Rajhans <anunay99@gmail.com>
2019-02-13 23:27:53 +0530

    Made socialLinksRecycler to load up properly.

--- app/src/main/java/org/fossasia/openevent/general/social/SocialLinksViewHolder.kt
+++ app/src/main/java/org/fossasia/openevent/general/social/SocialLinksViewHolder.kt
@@ -15,13 +15,10 @@ class SocialLinksViewHolder(itemView: View, private var context: Context) : Recy
 
     fun bind(socialLink: SocialLink) {
         val drawableId = getSocialLinkDrawableId(socialLink.name)
-        if (drawableId != -1) {
-            val imageDrawable: Drawable? = ContextCompat.getDrawable(context, drawableId)
-            imageDrawable?.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(context, R.color.greyMore),
-                PorterDuff.Mode.SRC_IN)
-
-            itemView.imgSocialLink.setImageDrawable(imageDrawable)
-        }
+        val imageDrawable: Drawable? = ContextCompat.getDrawable(context, drawableId)
+        imageDrawable?.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(context, R.color.greyMore),
+            PorterDuff.Mode.SRC_IN)
+        itemView.imgSocialLink.setImageDrawable(imageDrawable)
 
         itemView.setOnClickListener {
             Utils.openUrl(context, socialLink.link)
@@ -29,14 +26,44 @@ class SocialLinksViewHolder(itemView: View, private var context: Context) : Recy
     }
 
     private fun getSocialLinkDrawableId(name: String): Int {
-        return when (name) {
-            "Github Url" -> R.drawable.ic_github_24dp
-            "Twitter Url" -> R.drawable.ic_twitter_24dp
-            "Facebook Url" -> R.drawable.ic_facebook_24dp
-            "LinkedIn Url" -> R.drawable.ic_linkedin_24dp
-            "Youtube Url" -> R.drawable.ic_youtube_24dp
-            "Google Url" -> R.drawable.ic_google_plus_24dp
-            else -> -1
+        return when (name.toLowerCase()) {
+            "github url",
+            "githuburl",
+            "github" -> R.drawable.ic_github_24dp
+            "twitter url",
+            "twitterurl",
+            "twitter" -> R.drawable.ic_twitter_24dp
+            "facebook url",
+            "facebookurl",
+            "facebook" -> R.drawable.ic_facebook_24dp
+            "linkedin url",
+            "linkedinurl",
+            "linkedin" -> R.drawable.ic_linkedin_24dp
+            "youtube url",
+            "youtubeurl",
+            "youtube" -> R.drawable.ic_youtube_24dp
+            "googleplus",
+            "googleplus url",
+            "googleplusurl",
+            "google plus",
+            "google plusurl",
+            "google plus url",
+            "google url",
+            "googleurl",
+            "google" -> R.drawable.ic_google_plus_24dp
+            "wikipedia",
+            "wikipedia url",
+            "wikipediaurl" -> R.drawable.ic_wikipedia_24dp
+            "flickr",
+            "flickr url",
+            "flickrurl" -> R.drawable.ic_flickr_24dp
+            "blogger",
+            "blogger url",
+            "bloggerurl",
+            "blog",
+            "blog url",
+            "blogurl" -> R.drawable.ic_blogger_24dp
+            else -> R.drawable.ic_link_black_24dp
         }
     }
 }
