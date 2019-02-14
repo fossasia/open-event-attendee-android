aa70d68eda0a41d87429290e90c3a3829aff2d13
Anunay Rajhans <anunay99@gmail.com>
2019-02-15 00:12:41 +0530

    Passed name.toLowerCase() instead of name.

--- app/src/main/java/org/fossasia/openevent/general/social/SocialLinksViewHolder.kt
+++ app/src/main/java/org/fossasia/openevent/general/social/SocialLinksViewHolder.kt
@@ -14,7 +14,7 @@ import org.fossasia.openevent.general.utils.Utils
 class SocialLinksViewHolder(itemView: View, private var context: Context) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(socialLink: SocialLink) {
-        val drawableId = getSocialLinkDrawableId(socialLink.name)
+        val drawableId = getSocialLinkDrawableId(socialLink.name.toLowerCase())
         val imageDrawable: Drawable? = ContextCompat.getDrawable(context, drawableId)
         imageDrawable?.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(context, R.color.greyMore),
             PorterDuff.Mode.SRC_IN)
@@ -26,15 +26,15 @@ class SocialLinksViewHolder(itemView: View, private var context: Context) : Recy
     }
 
     private fun getSocialLinkDrawableId(name: String): Int {
-        if (name.toLowerCase().contains("github")) return R.drawable.ic_github
-        else if (name.toLowerCase().contains("twitter")) return R.drawable.ic_twitter
-        else if (name.toLowerCase().contains("facebook")) return R.drawable.ic_facebook
-        else if (name.toLowerCase().contains("linkedin")) return R.drawable.ic_linkedin
-        else if (name.toLowerCase().contains("youtube")) return R.drawable.ic_youtube
-        else if (name.toLowerCase().contains("google")) return R.drawable.ic_google_plus
-        else if (name.toLowerCase().contains("wiki")) return R.drawable.ic_wikipedia
-        else if (name.toLowerCase().contains("flickr")) return R.drawable.ic_flickr
-        else if (name.toLowerCase().contains("blog")) return R.drawable.ic_blogger
+        if (name.contains("github")) return R.drawable.ic_github
+        else if (name.contains("twitter")) return R.drawable.ic_twitter
+        else if (name.contains("facebook")) return R.drawable.ic_facebook
+        else if (name.contains("linkedin")) return R.drawable.ic_linkedin
+        else if (name.contains("youtube")) return R.drawable.ic_youtube
+        else if (name.contains("google")) return R.drawable.ic_google_plus
+        else if (name.contains("wiki")) return R.drawable.ic_wikipedia
+        else if (name.contains("flickr")) return R.drawable.ic_flickr
+        else if (name.contains("blog")) return R.drawable.ic_blogger
         else return R.drawable.ic_link_black
     }
 }
