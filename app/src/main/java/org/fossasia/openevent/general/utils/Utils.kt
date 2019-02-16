affc0df8809c62e55e5e34ff5203bd415bc44922
Anunay Rajhans <anunay99@gmail.com>
2019-02-16 11:28:46 +0530

    Spotless fixed.

--- app/src/main/java/org/fossasia/openevent/general/utils/Utils.kt
+++ app/src/main/java/org/fossasia/openevent/general/utils/Utils.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.view.View
-import android.view.Window
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.DrawableRes
