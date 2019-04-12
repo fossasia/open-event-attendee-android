4aad53362299c1475f1f00dce63674984ca3a3e1
thelimitbreaker <rohanmaityofficial@gmail.com>
2019-04-12 21:43:40 +0530

    MapBox AutoComplete Details - Add MapBox Java lib - Make Popular Locations Hide when Place Suggestions come and Vice-versa - Add debounce to place query newtwork request (to avoid unnecessary calls) - Add english as default language for Suggesstions

--- app/src/main/java/org/fossasia/openevent/general/search/SearchLocationFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/search/SearchLocationFragment.kt
@@ -15,17 +15,24 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
-import kotlinx.android.synthetic.main.fragment_search_location.view.currentLocationLinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.reactivex.subjects.PublishSubject
+import kotlinx.android.synthetic.main.fragment_search_location.eventLocationLv
+import kotlinx.android.synthetic.main.fragment_search_location.locationSearchView
+import kotlinx.android.synthetic.main.fragment_search_location.placeSuggestionsCard
+import kotlinx.android.synthetic.main.fragment_search_location.titleTv
+import kotlinx.android.synthetic.main.fragment_search_location.view.currentLocation
+import kotlinx.android.synthetic.main.fragment_search_location.view.eventLocationLv
 import kotlinx.android.synthetic.main.fragment_search_location.view.locationProgressBar
 import kotlinx.android.synthetic.main.fragment_search_location.view.locationSearchView
-import kotlinx.android.synthetic.main.fragment_search_location.view.eventLocationLv
+import kotlinx.android.synthetic.main.fragment_search_location.view.rvAutoPlaces
 import kotlinx.android.synthetic.main.fragment_search_location.view.shimmerSearchEventTypes
 import org.fossasia.openevent.general.R
 import org.fossasia.openevent.general.utils.Utils
-import org.fossasia.openevent.general.utils.extensions.nonNull
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.fossasia.openevent.general.utils.Utils.setToolbar
+import org.fossasia.openevent.general.utils.extensions.nonNull
 import org.jetbrains.anko.design.snackbar
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 const val LOCATION_PERMISSION_REQUEST = 1000
 
@@ -36,62 +43,38 @@ class SearchLocationFragment : Fragment() {
     private val safeArgs: SearchLocationFragmentArgs by navArgs()
     private val eventLocationList: MutableList<String> = ArrayList()
 
+    private val placeSuggestionsAdapter = PlaceSuggestionsAdapter()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(R.layout.fragment_search_location, container, false)
         setToolbar(activity, hasUpEnabled = true, show = true)
         setHasOptionsMenu(true)
         searchLocationViewModel.loadEventsLocation()
-        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, eventLocationList)
-        rootView.eventLocationLv.adapter = adapter
-        searchLocationViewModel.showShimmer
-            .nonNull()
-            .observe(viewLifecycleOwner, Observer { shouldShowShimmer ->
-                if (shouldShowShimmer) {
-                    rootView.shimmerSearchEventTypes.startShimmer()
-                    adapter.clear()
-                } else {
-                    rootView.shimmerSearchEventTypes.stopShimmer()
-                }
-                rootView.shimmerSearchEventTypes.isVisible = shouldShowShimmer
-            })
-        searchLocationViewModel.eventLocations
-            .nonNull()
-            .observe(this, Observer { list ->
-                list.forEach {
-                    eventLocationList.add(it.name ?: "")
-                }
-                adapter.notifyDataSetChanged()
-            })
-        rootView.eventLocationLv.setOnItemClickListener { parent, view, position, id ->
-            searchLocationViewModel.saveSearch(eventLocationList[position])
-            redirectToMain()
-        }
+
+        setupPopularLocations()
+
+        setUpLocationSearchView()
+
+        setupRecyclerPlaceSuggestions()
 
         geoLocationViewModel.currentLocationVisibility.observe(viewLifecycleOwner, Observer {
-            rootView.currentLocationLinearLayout.visibility = View.GONE
+            rootView.currentLocation.isVisible = false
         })
 
-        rootView.currentLocationLinearLayout.setOnClickListener {
+        rootView.currentLocation.setOnClickListener {
             checkLocationPermission()
             geoLocationViewModel.configure()
             rootView.locationProgressBar.visibility = View.VISIBLE
         }
 
         geoLocationViewModel.location.observe(viewLifecycleOwner, Observer { location ->
-            searchLocationViewModel.saveSearch(location)
-            redirectToMain()
+            savePlaceAndRedirectToMain(location)
         })
 
-        rootView.locationSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                searchLocationViewModel.saveSearch(query)
-                redirectToMain()
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String): Boolean {
-                return false
-            }
+        searchLocationViewModel.placeSuggestions.observe(viewLifecycleOwner, Observer {
+            placeSuggestionsAdapter.submitList(it)
+            // To handle the case : search result comes after query is empty
+            toggleSuggestionVisibility(it.isNotEmpty() && locationSearchView.query.isNotEmpty())
         })
 
         return rootView
@@ -137,6 +120,81 @@ class SearchLocationFragment : Fragment() {
                     rootView.locationProgressBar.visibility = View.GONE
                 }
             }
+        }
+    }
+
+    private fun setupRecyclerPlaceSuggestions() {
+        rootView.rvAutoPlaces.layoutManager = LinearLayoutManager(context)
+        rootView.rvAutoPlaces.adapter = placeSuggestionsAdapter
+
+        placeSuggestionsAdapter.onSuggestionClick = {
+            savePlaceAndRedirectToMain(it)
+        }
+    }
+    private fun toggleSuggestionVisibility(state: Boolean) {
+        placeSuggestionsCard.isVisible = state
+
+        titleTv.isVisible = !state
+        eventLocationLv.isVisible = !state
+    }
+
+    private fun savePlaceAndRedirectToMain(place: String) {
+        searchLocationViewModel.saveSearch(place)
+        redirectToMain()
+    }
+
+    private fun setUpLocationSearchView() {
+        val subject = PublishSubject.create<String>()
+        rootView.locationSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                handleDisplayPlaceSuggestions(query, subject)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                handleDisplayPlaceSuggestions(newText, subject)
+                return false
+            }
+        })
+        searchLocationViewModel.handlePlaceSuggestions(subject)
+    }
+
+    private fun handleDisplayPlaceSuggestions(query: String, subject: PublishSubject<String>) {
+        if (query.isNotEmpty()) {
+            subject.onNext(query)
+        } else {
+            toggleSuggestionVisibility(false)
+        }
+    }
+
+    private fun setupPopularLocations() {
+
+        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, eventLocationList)
+        rootView.eventLocationLv.adapter = adapter
+
+        searchLocationViewModel.showShimmer
+            .nonNull()
+            .observe(viewLifecycleOwner, Observer { shouldShowShimmer ->
+                if (shouldShowShimmer) {
+                    rootView.shimmerSearchEventTypes.startShimmer()
+                    adapter.clear()
+                } else {
+                    rootView.shimmerSearchEventTypes.stopShimmer()
+                }
+                rootView.shimmerSearchEventTypes.isVisible = shouldShowShimmer
+            })
+
+        searchLocationViewModel.eventLocations
+            .nonNull()
+            .observe(this, Observer { list ->
+                list.forEach {
+                    eventLocationList.add(it.name)
+                }
+                adapter.notifyDataSetChanged()
+            })
+
+        rootView.eventLocationLv.setOnItemClickListener { parent, view, position, id ->
+            savePlaceAndRedirectToMain(eventLocationList[position])
         }
     }
 }
