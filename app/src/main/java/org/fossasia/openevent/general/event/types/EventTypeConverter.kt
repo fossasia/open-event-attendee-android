81752474f02141ebc23bc7efd10f731fc7bdd97e
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-04-21 20:43:17 +0530

    fix: fix query,use jacksonObjectMapper and view holder crash

--- app/src/main/java/org/fossasia/openevent/general/event/types/EventTypeConverter.kt
+++ app/src/main/java/org/fossasia/openevent/general/event/types/EventTypeConverter.kt
@@ -2,11 +2,12 @@ package org.fossasia.openevent.general.event.types
 
 import androidx.room.TypeConverter
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 class EventTypeConverter {
     @TypeConverter
     fun toEventType(json: String): EventType? {
-        return ObjectMapper().readerFor(EventType::class.java).readValue<EventType>(json)
+        return jacksonObjectMapper().readerFor(EventType::class.java).readValue<EventType>(json)
     }
 
     @TypeConverter
