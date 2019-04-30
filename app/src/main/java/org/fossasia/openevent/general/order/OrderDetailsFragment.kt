a91c4ed1d08e0828fb38ffb5efb245a0555bf874
Duc Le Tran <letrananhduc0311@gmail.com>
2019-04-30 18:55:43 +0200

    Summary Change: Add sharing ticket feature
    Detail:
    - Create a bitmap from container view of single ticket
    - Turn bitmap into an URL and then share it to another app
    
    Fixes: #1699

--- app/src/main/java/org/fossasia/openevent/general/order/OrderDetailsFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/order/OrderDetailsFragment.kt
@@ -1,10 +1,17 @@
 package org.fossasia.openevent.general.order
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -12,9 +19,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_order_details.view.orderDetailCoordinatorLayout
 import kotlinx.android.synthetic.main.fragment_order_details.view.orderDetailsRecycler
 import kotlinx.android.synthetic.main.fragment_order_details.view.progressBar
+import kotlinx.android.synthetic.main.item_card_order_details.view.orderDetailCardView
+import org.fossasia.openevent.general.BuildConfig
 import org.fossasia.openevent.general.R
 import org.fossasia.openevent.general.event.EventDetailsFragmentArgs
 import org.fossasia.openevent.general.utils.Utils.getAnimFade
@@ -23,6 +33,9 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 import org.fossasia.openevent.general.utils.Utils.setToolbar
 import org.jetbrains.anko.design.longSnackbar
+import org.jetbrains.anko.design.snackbar
+import java.io.File
+import java.io.FileOutputStream
 
 class OrderDetailsFragment : Fragment() {
 
@@ -69,7 +82,20 @@ class OrderDetailsFragment : Fragment() {
         linearLayoutManager = LinearLayoutManager(context)
         linearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
         rootView.orderDetailsRecycler.layoutManager = linearLayoutManager
-        LinearSnapHelper().attachToRecyclerView(rootView.orderDetailsRecycler)
+        val snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(rootView.orderDetailsRecycler)
+        rootView.orderDetailsRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    val centerView = snapHelper.findSnapView(linearLayoutManager)
+                    centerView?.let {
+                        val itemPosition = linearLayoutManager.getPosition(it)
+                        orderDetailsViewModel.currentTicketPosition = itemPosition
+                    }
+                }
+            }
+        })
 
         val eventDetailsListener = object : OrderDetailsRecyclerAdapter.EventDetailsListener {
             override fun onClick(eventID: Long) {
@@ -101,13 +127,59 @@ class OrderDetailsFragment : Fragment() {
         return rootView
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.order_detail, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
                 activity?.onBackPressed()
                 true
             }
+            R.id.share_ticket -> {
+                shareCurrentTicket()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun shareCurrentTicket() {
+        val currentTicketViewHolder =
+            rootView.orderDetailsRecycler.findViewHolderForAdapterPosition(orderDetailsViewModel.currentTicketPosition)
+        if (currentTicketViewHolder != null && currentTicketViewHolder is OrderDetailsViewHolder) {
+            val bitmap = getBitmapFromView(currentTicketViewHolder.itemView.rootView.orderDetailCardView)
+            val bitmapUri = getBitmapUri(bitmap)
+            if (bitmapUri == null) {
+                rootView.snackbar(getString(R.string.fail_sharing_ticket))
+                return
+            }
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.type = "image/png"
+            intent.putExtra(Intent.EXTRA_STREAM, bitmapUri)
+            startActivity(intent)
+        } else {
+            rootView.snackbar(getString(R.string.fail_sharing_ticket))
+        }
+    }
+
+    private fun getBitmapUri(bitmap: Bitmap): Uri? {
+        val myContext = context ?: return null
+        val file = File(myContext.cacheDir, "shared_image.png")
+        return FileOutputStream(file)
+            .use { fileOutputStream ->
+                bitmap.compress(Bitmap.CompressFormat.PNG, 90, fileOutputStream)
+                FileProvider.getUriForFile(myContext, BuildConfig.APPLICATION_ID + ".provider", file)
+            }
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
     }
 }
