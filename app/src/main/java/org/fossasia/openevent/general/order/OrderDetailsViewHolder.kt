bc9508451ac5ebd1706708f77b848f2b416ac348
Duc Le Tran <36192582+anhanh11001@users.noreply.github.com>
2019-05-10 06:11:57 +0200

    feat: Add enlarged QR Image + Increase brightness (#1727)
    Detail:
    - Set up click listener to enlarge QR Image
    - Increate brightness when clicking on QR Image.
    - Setting margin of created QR image small so that QR Image Dialog can contain large image.
    
    Fixes: #1215

--- app/src/main/java/org/fossasia/openevent/general/order/OrderDetailsViewHolder.kt
+++ app/src/main/java/org/fossasia/openevent/general/order/OrderDetailsViewHolder.kt
@@ -32,67 +32,70 @@ class OrderDetailsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
         attendee: Attendee,
         event: Event?,
         orderIdentifier: String?,
-        eventDetailsListener: OrderDetailsRecyclerAdapter.EventDetailsListener?
+        eventDetailsListener: OrderDetailsRecyclerAdapter.EventDetailsListener?,
+        qrImageClickListener: OrderDetailsRecyclerAdapter.QrImageClickListener?
     ) {
-        if (event != null) {
-            val formattedDateTime = EventUtils.getEventDateTime(event.startsAt, event.timezone)
-            val formattedDate = EventUtils.getFormattedDateShort(formattedDateTime)
-            val formattedTime = EventUtils.getFormattedTime(formattedDateTime)
-            val timezone = EventUtils.getFormattedTimeZone(formattedDateTime)
+        if (event == null) return
+        val formattedDateTime = EventUtils.getEventDateTime(event.startsAt, event.timezone)
+        val formattedDate = EventUtils.getFormattedDateShort(formattedDateTime)
+        val formattedTime = EventUtils.getFormattedTime(formattedDateTime)
+        val timezone = EventUtils.getFormattedTimeZone(formattedDateTime)
 
-            itemView.eventName.text = event.name
-            itemView.location.text = event.locationName
-            itemView.date.text = "$formattedDate\n$formattedTime $timezone"
-            itemView.eventSummary.text = event.description?.stripHtml()
+        itemView.eventName.text = event.name
+        itemView.location.text = event.locationName
+        itemView.date.text = "$formattedDate\n$formattedTime $timezone"
+        itemView.eventSummary.text = event.description?.stripHtml()
 
-            if (event.organizerName.isNullOrEmpty()) {
-                itemView.organizerLabel.visibility = View.GONE
-            } else {
-                itemView.organizer.text = event.organizerName
+        if (event.organizerName.isNullOrEmpty()) {
+            itemView.organizerLabel.visibility = View.GONE
+        } else {
+            itemView.organizer.text = event.organizerName
+        }
+        itemView.map.setOnClickListener {
+            val mapUrl = loadMapUrl(event)
+            val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(mapUrl))
+            val packageManager = itemView.context?.packageManager
+            if (packageManager != null && mapIntent.resolveActivity(packageManager) != null) {
+                itemView.context.startActivity(mapIntent)
             }
-            itemView.map.setOnClickListener {
-                val mapUrl = loadMapUrl(event)
-                val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse(mapUrl))
-                val packageManager = itemView.context?.packageManager
-                if (packageManager != null && mapIntent.resolveActivity(packageManager) != null) {
-                    itemView.context.startActivity(mapIntent)
-                }
+        }
+        if (!attendee.pdfUrl.isNullOrBlank()) {
+            itemView.downloadButton.isEnabled = true
+            itemView.downloadButton.setOnClickListener {
+                itemView.context.browse(attendee.pdfUrl)
             }
-            if (!attendee.pdfUrl.isNullOrBlank()) {
-                itemView.downloadButton.isEnabled = true
-                itemView.downloadButton.setOnClickListener {
-                    itemView.context.browse(attendee.pdfUrl)
-                }
-            }
+        }
 
-            itemView.calendar.setOnClickListener {
-                val intent = Intent(Intent.ACTION_INSERT)
-                intent.type = "vnd.android.cursor.item/event"
-                intent.putExtra(CalendarContract.Events.TITLE, event.name)
-                intent.putExtra(CalendarContract.Events.DESCRIPTION, event.description?.stripHtml())
-                intent.putExtra(CalendarContract.Events.EVENT_LOCATION, event.locationName)
-                intent.putExtra(CalendarContract.Events.CALENDAR_TIME_ZONE, event.timezone)
-                intent.putExtra(
-                    CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-                    EventUtils.getTimeInMilliSeconds(event.startsAt, event.timezone))
-                intent.putExtra(
-                    CalendarContract.EXTRA_EVENT_END_TIME,
-                    EventUtils.getTimeInMilliSeconds(event.endsAt, event.timezone))
-                itemView.context.startActivity(intent)
-            }
+        itemView.calendar.setOnClickListener {
+            val intent = Intent(Intent.ACTION_INSERT)
+            intent.type = "vnd.android.cursor.item/event"
+            intent.putExtra(CalendarContract.Events.TITLE, event.name)
+            intent.putExtra(CalendarContract.Events.DESCRIPTION, event.description?.stripHtml())
+            intent.putExtra(CalendarContract.Events.EVENT_LOCATION, event.locationName)
+            intent.putExtra(CalendarContract.Events.CALENDAR_TIME_ZONE, event.timezone)
+            intent.putExtra(
+                CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                EventUtils.getTimeInMilliSeconds(event.startsAt, event.timezone))
+            intent.putExtra(
+                CalendarContract.EXTRA_EVENT_END_TIME,
+                EventUtils.getTimeInMilliSeconds(event.endsAt, event.timezone))
+            itemView.context.startActivity(intent)
+        }
 
-            itemView.eventDetails.setOnClickListener {
-                eventDetailsListener?.onClick(event.id)
-            }
+        itemView.eventDetails.setOnClickListener {
+            eventDetailsListener?.onClick(event.id)
         }
 
         itemView.name.text = "${attendee.firstname} ${attendee.lastname}"
         val ticketIdentifier = "$orderIdentifier-${attendee.id}"
         itemView.orderIdentifier.text = ticketIdentifier
-        val bitmap = qrCode.generateQrBitmap(ticketIdentifier, 200, 200)
+        val bitmap = qrCode.generateQrBitmap(ticketIdentifier, 400, 400)
 
         if (bitmap != null) {
             itemView.qrCodeView.setImageBitmap(bitmap)
+            itemView.qrCodeView.setOnClickListener {
+                qrImageClickListener?.onClick(bitmap)
+            }
         } else {
             itemView.qrCodeView.visibility = View.GONE
         }
