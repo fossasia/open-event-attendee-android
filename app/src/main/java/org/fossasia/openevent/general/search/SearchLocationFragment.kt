a4b9945191119a236fbf65a5f90e2dc620b783e6
Duc Le Tran <letrananhduc0311@gmail.com>
2019-06-22 09:43:17 +0200

    Summary: Add recent search query
    Details:
    - Save recent search location by Preference
    - Improve UI in SearchLocationFragment
    
    Fixes: #575

--- app/src/main/java/org/fossasia/openevent/general/search/SearchLocationFragment.kt
+++ app/src/main/java/org/fossasia/openevent/general/search/SearchLocationFragment.kt
@@ -6,22 +6,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.fragment_search_location.eventLocationLv
-import kotlinx.android.synthetic.main.fragment_search_location.locationSearchView
-import kotlinx.android.synthetic.main.fragment_search_location.placeSuggestionsCard
-import kotlinx.android.synthetic.main.fragment_search_location.titleTv
+import kotlinx.android.synthetic.main.fragment_search_location.view.scrollView
+import kotlinx.android.synthetic.main.fragment_search_location.view.toolbarTitle
+import kotlinx.android.synthetic.main.fragment_search_location.view.toolbarLayout
+import kotlinx.android.synthetic.main.fragment_search_location.view.placeSuggestionsCard
+import kotlinx.android.synthetic.main.fragment_search_location.view.recentSearchLayout
+import kotlinx.android.synthetic.main.fragment_search_location.view.recentSearchRv
+import kotlinx.android.synthetic.main.fragment_search_location.view.popularLocationsLayout
 import kotlinx.android.synthetic.main.fragment_search_location.view.currentLocation
-import kotlinx.android.synthetic.main.fragment_search_location.view.eventLocationLv
+import kotlinx.android.synthetic.main.fragment_search_location.view.popularLocationsRv
 import kotlinx.android.synthetic.main.fragment_search_location.view.locationProgressBar
 import kotlinx.android.synthetic.main.fragment_search_location.view.locationSearchView
 import kotlinx.android.synthetic.main.fragment_search_location.view.rvAutoPlaces
@@ -31,6 +35,7 @@ import org.fossasia.openevent.general.R
 import org.fossasia.openevent.general.utils.Utils
 import org.fossasia.openevent.general.utils.Utils.isLocationEnabled
 import org.fossasia.openevent.general.utils.Utils.setToolbar
+import org.fossasia.openevent.general.utils.Utils.showSoftKeyboard
 import org.fossasia.openevent.general.utils.extensions.nonNull
 import org.jetbrains.anko.design.snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -42,20 +47,22 @@ class SearchLocationFragment : Fragment() {
     private val searchLocationViewModel by viewModel<SearchLocationViewModel>()
     private val geoLocationViewModel by viewModel<GeoLocationViewModel>()
     private val safeArgs: SearchLocationFragmentArgs by navArgs()
-    private val eventLocationList: MutableList<String> = ArrayList()
+    private val popularLocationAdapter = LocationsAdapter()
+    private val recentLocationAdapter = LocationsAdapter()
 
     private val placeSuggestionsAdapter = PlaceSuggestionsAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = inflater.inflate(R.layout.fragment_search_location, container, false)
         setToolbar(activity, show = false)
-        searchLocationViewModel.loadEventsLocation()
 
         setupPopularLocations()
 
         setUpLocationSearchView()
 
         setupRecyclerPlaceSuggestions()
+
+        setupRecentSearchLocations()
 
         geoLocationViewModel.currentLocationVisibility.observe(viewLifecycleOwner, Observer {
             rootView.currentLocation.isVisible = false
@@ -76,7 +83,7 @@ class SearchLocationFragment : Fragment() {
         searchLocationViewModel.placeSuggestions.observe(viewLifecycleOwner, Observer {
             placeSuggestionsAdapter.submitList(it)
             // To handle the case : search result comes after query is empty
-            toggleSuggestionVisibility(it.isNotEmpty() && locationSearchView.query.isNotEmpty())
+            toggleSuggestionVisibility(it.isNotEmpty() && rootView.locationSearchView.query.isNotEmpty())
         })
 
         rootView.toolbar.setNavigationOnClickListener {
@@ -87,9 +94,36 @@ class SearchLocationFragment : Fragment() {
         return rootView
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val listener = object : TextClickListener {
+            override fun onTextClick(location: String) {
+                savePlaceAndRedirectToMain(location)
+            }
+        }
+        popularLocationAdapter.setListener(listener)
+        recentLocationAdapter.setListener(listener)
+
+        rootView.toolbarTitle.setOnClickListener {
+            rootView.scrollView.scrollTo(0, 0)
+            rootView.locationSearchView.isFocusable = true
+            showSoftKeyboard(context, rootView)
+        }
+
+        rootView.scrollView.setOnScrollChangeListener { _: NestedScrollView?, _: Int, scrollY: Int, _: Int, _: Int ->
+            if (scrollY > rootView.locationSearchView.y) {
+                rootView.toolbarLayout.elevation = resources.getDimension(R.dimen.custom_toolbar_elevation)
+                rootView.toolbarTitle.text = getString(R.string.location_hint)
+            } else {
+                rootView.toolbarLayout.elevation = 0F
+                rootView.toolbarTitle.text = ""
+            }
+        }
+    }
+
     override fun onResume() {
         super.onResume()
-        Utils.showSoftKeyboard(context, rootView.locationSearchView)
+        showSoftKeyboard(context, rootView.locationSearchView)
     }
 
     private fun checkLocationPermission() {
@@ -132,10 +166,10 @@ class SearchLocationFragment : Fragment() {
         }
     }
     private fun toggleSuggestionVisibility(state: Boolean) {
-        placeSuggestionsCard.isVisible = state
+        rootView.placeSuggestionsCard.isVisible = state
 
-        titleTv.isVisible = !state
-        eventLocationLv.isVisible = !state
+        rootView.popularLocationsLayout.isVisible = !state && popularLocationAdapter.itemCount != 0
+        rootView.recentSearchLayout.isVisible = !state && recentLocationAdapter.itemCount != 0
     }
 
     private fun savePlaceAndRedirectToMain(place: String) {
@@ -167,17 +201,27 @@ class SearchLocationFragment : Fragment() {
         }
     }
 
-    private fun setupPopularLocations() {
+    private fun setupRecentSearchLocations() {
+        val recentSearch = searchLocationViewModel.getRecentLocationList()
+        if (recentSearch.isEmpty()) {
+            rootView.recentSearchLayout.isVisible = false
+        } else {
+            rootView.recentSearchLayout.isVisible = true
+            recentLocationAdapter.addAll(recentSearch)
+            rootView.recentSearchRv.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            rootView.recentSearchRv.adapter = recentLocationAdapter
+        }
+    }
 
-        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, eventLocationList)
-        rootView.eventLocationLv.adapter = adapter
+    private fun setupPopularLocations() {
+        rootView.popularLocationsRv.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rootView.popularLocationsRv.adapter = popularLocationAdapter
 
         searchLocationViewModel.showShimmer
             .nonNull()
             .observe(viewLifecycleOwner, Observer { shouldShowShimmer ->
                 if (shouldShowShimmer) {
                     rootView.shimmerSearchEventTypes.startShimmer()
-                    adapter.clear()
                 } else {
                     rootView.shimmerSearchEventTypes.stopShimmer()
                 }
@@ -187,14 +231,10 @@ class SearchLocationFragment : Fragment() {
         searchLocationViewModel.eventLocations
             .nonNull()
             .observe(this, Observer { list ->
-                list.forEach {
-                    eventLocationList.add(it.name)
-                }
-                adapter.notifyDataSetChanged()
+                popularLocationAdapter.addAll(list.map { it.name })
+                rootView.popularLocationsLayout.isVisible = list.isNotEmpty()
             })
 
-        rootView.eventLocationLv.setOnItemClickListener { parent, view, position, id ->
-            savePlaceAndRedirectToMain(eventLocationList[position])
-        }
+        searchLocationViewModel.loadEventsLocation()
     }
 }
