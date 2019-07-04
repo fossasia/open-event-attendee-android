de63ce0f3993b5a0fe2c2426959c4cc9c853edb6
liveHarshit <live.harshit@hotmail.com>
2019-07-04 13:11:50 +0530

    update prep-key script

--- scripts/prep-key.sh
+++ scripts/prep-key.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
-export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
+export DEPLOY_BRANCH=${DEPLOY_BRANCH:-development}
+export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-android" -o "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-attendee-android" ] || ! [ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" -o "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     echo "We decrypt key only for pushes to the master branch and not PRs. So, skip."
     exit 0
 fi
