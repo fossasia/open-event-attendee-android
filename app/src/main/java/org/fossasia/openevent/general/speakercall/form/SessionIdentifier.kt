d74d9068976b91fd6fb3b07cb44fbfa164574e82
Duc Le Tran <letrananhduc0311@gmail.com>
2019-08-03 16:21:19 +0700

    Summary: Add custom form for speakers/sessions
    Fixes: #2166

---
+++ app/src/main/java/org/fossasia/openevent/general/speakercall/form/SessionIdentifier.kt
@@ -0,0 +1,12 @@
+package org.fossasia.openevent.general.speakercall.form
+
+object SessionIdentifier {
+    val TITLE = "title"
+    val SUBTITLE = "subtitle"
+    val SHORT_ABSTRACT = "shortAbstract"
+    val LONG_ABSTRACT = "longAbstract"
+    val COMMENTS = "comments"
+    val TRACK = "track"
+    val SESSION_TYPE = "session_type"
+    val LANGUAGE = "language"
+}
---
+++ app/src/main/java/org/fossasia/openevent/general/speakercall/form/SpeakerIdentifier.kt
@@ -0,0 +1,20 @@
+package org.fossasia.openevent.general.speakercall.form
+
+object SpeakerIdentifier {
+    val NAME = "name"
+    val EMAIL = "email"
+    val PHOTO = "photoUrl"
+    val ORGANIZATION = "organisation"
+    val POSITION = "position"
+    val COUNTRY = "country"
+    val SHORT_BIO = "shortBiography"
+    val LONG_BIO = "longBiography"
+    val MOBILE = "mobile"
+    val WEBSITE = "website"
+    val FACEBOOK = "facebook"
+    val TWITTER = "twitter"
+    val GITHUB = "github"
+    val LINKEDIN = "linkedin"
+    val SPEAKING_EXPERIENCE = "speakingExperience"
+    val HEARD_FROM = "heardFrom"
+}
