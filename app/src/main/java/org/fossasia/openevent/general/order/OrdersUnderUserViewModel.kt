2ab9825959777ad4194eea2b6be76657d98b506e
aryanguls <58459496+aryanguls@users.noreply.github.com>
2020-01-21 12:29:28 +0530

    Update OrdersUnderUserViewModel.kt

--- app/src/main/java/org/fossasia/openevent/general/order/OrdersUnderUserViewModel.kt
+++ app/src/main/java/org/fossasia/openevent/general/order/OrdersUnderUserViewModel.kt
@@ -9,11 +9,15 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.schedulers.Schedulers
+import org.fossasia.openevent.general.R
+import org.fossasia.openevent.general.attendees.ORDER_STATUS_COMPLETED
 import org.fossasia.openevent.general.auth.AuthHolder
 import org.fossasia.openevent.general.common.SingleLiveEvent
 import org.fossasia.openevent.general.connectivity.MutableConnectionLiveData
+import org.fossasia.openevent.general.data.Resource
 import org.fossasia.openevent.general.event.Event
 import org.fossasia.openevent.general.event.EventService
+import org.fossasia.openevent.general.utils.extensions.withDefaultSchedulers
 import timber.log.Timber
 
 class OrdersUnderUserViewModel(
@@ -21,7 +25,8 @@ class OrdersUnderUserViewModel(
     private val eventService: EventService,
     private val authHolder: AuthHolder,
     private val mutableConnectionLiveData: MutableConnectionLiveData,
-    private val config: PagedList.Config
+    private val config: PagedList.Config,
+    private val resource: Resource
 ) : ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
@@ -38,6 +43,8 @@ class OrdersUnderUserViewModel(
 
     // Retain filter options
     val filter = OrderFilter()
+    
+    private lateinit var confirmOrder: ConfirmOrder
 
     fun getId() = authHolder.getId()
 
@@ -46,45 +53,78 @@ class OrdersUnderUserViewModel(
     fun getOrdersAndEventsOfUser(showExpired: Boolean, fromDb: Boolean) {
 
         val sourceFactory = OrderDataSourceFactory(
-            orderService,
-            eventService,
-            compositeDisposable,
-            showExpired,
-            mutableShowShimmerResults,
-            mutableNumOfTickets,
-            mutableMessage,
-            getId(),
-            filter,
-            fromDb
+                orderService,
+                eventService,
+                compositeDisposable,
+                showExpired,
+                mutableShowShimmerResults,
+                mutableNumOfTickets,
+                mutableMessage,
+                getId(),
+                filter,
+                fromDb
         )
 
         val ordersAndEventsPagedList = RxPagedListBuilder(sourceFactory, config)
-            .setFetchScheduler(Schedulers.io())
-            .buildObservable()
-            .cache()
+                .setFetchScheduler(Schedulers.io())
+                .buildObservable()
+                .cache()
 
         compositeDisposable += ordersAndEventsPagedList
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .distinctUntilChanged()
-            .doOnSubscribe {
-                mutableShowShimmerResults.value = true
-            }.subscribe({
-                val currentPagedOrdersAndEvents = mutableEventAndOrderPaged.value
-                if (currentPagedOrdersAndEvents == null) {
-                    mutableEventAndOrderPaged.value = it
-                } else {
-                    currentPagedOrdersAndEvents.addAll(it)
-                    mutableEventAndOrderPaged.value = currentPagedOrdersAndEvents
-                }
-            }, {
-                Timber.d(it, "Failed  to list events under a user ")
-            })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .distinctUntilChanged()
+                .doOnSubscribe {
+                    mutableShowShimmerResults.value = true
+                }.subscribe({
+                    val currentPagedOrdersAndEvents = mutableEventAndOrderPaged.value
+                    if (currentPagedOrdersAndEvents == null) {
+                        mutableEventAndOrderPaged.value = it
+                    } else {
+                        currentPagedOrdersAndEvents.addAll(it)
+                        mutableEventAndOrderPaged.value = currentPagedOrdersAndEvents
+                    }
+                }, {
+                    Timber.d(it, "Failed  to list events under a user ")
+                })
     }
 
     fun clearOrders() {
         mutableEventAndOrderPaged.value = null
         mutableNumOfTickets.value = 0
+    }
+    
+    fun sendPaypalConfirm(paymentId: String, pendingOrder: Order) {
+        pendingOrder.let { order ->
+            compositeDisposable += orderService.verifyPaypalPayment(order.identifier.toString(), paymentId)
+                    .withDefaultSchedulers()
+                    .doOnSubscribe {
+                    }.subscribe({
+                        if (it.status) {
+                            confirmOrder = ConfirmOrder(order.id.toString(), ORDER_STATUS_COMPLETED)
+                            confirmOrderStatus(order.identifier.toString(), confirmOrder)
+                        } else {
+                            mutableMessage.value = it.error
+                        }
+                    }, {
+                        mutableMessage.value = resource.getString(R.string.error_making_paypal_payment_message)
+                        Timber.e(it, "Error verifying paypal payment")
+                    })
+        }
+    }
+
+    private fun confirmOrderStatus(identifier: String, order: ConfirmOrder) {
+        compositeDisposable += orderService.confirmOrder(identifier, order)
+                .withDefaultSchedulers()
+                .doFinally {
+                }.subscribe({
+                    mutableMessage.value = resource.getString(R.string.order_success_message)
+                    Timber.d("Updated order status successfully !")
+                    // orderCompleted.value = true
+                }, {
+                    mutableMessage.value = resource.getString(R.string.order_fail_message)
+                    Timber.d(it, "Failed updating order status")
+                })
     }
 
     fun isConnected(): Boolean = mutableConnectionLiveData.value ?: false
